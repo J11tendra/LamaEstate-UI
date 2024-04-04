dea09fa5d4b1a6c5a1c23b23ba7c57d1fcc0b5a6
Jitendra <info.jiitendra@gmail.com>
2024-04-04 21:13:37 +0530

    feat: Add UserProfile Button

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom";
 import "./navbar.scss";
 import { useState } from "react";
 
 function Navbar() {
   const [open, setOpen] = useState(false);
+  const user = true;
+
   return (
     <nav>
       <div className="left">
@@ -16,10 +19,26 @@ function Navbar() {
         <a href="/">Agents</a>
       </div>
       <div className="right">
-        <a href="/">Sign in</a>
-        <a href="/" className="register">
-          Sign up
-        </a>
+        {user ? (
+          <div className="user">
+            <img
+              src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+              alt=""
+            />
+            <span>John Dog</span>
+            <Link to="/profile" className="profile">
+              <div className="notification">3</div>
+              <span>Profile</span>
+            </Link>
+          </div>
+        ) : (
+          <>
+            <a href="/">Sign in</a>
+            <a href="/" className="register">
+              Sign up
+            </a>
+          </>
+        )}
         <div className="menuIcon">
           <img
             src="/menu.png"
