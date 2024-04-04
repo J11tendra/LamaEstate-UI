721317f374cf31d74898cb6e2bea16a2bae6ace9
Jitendra <info.jiitendra@gmail.com>
2024-04-04 23:10:07 +0530

    feat: Add ProfilePage

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Layout from "./routes/layout/layout";
 import HomePage from "./routes/homePage/homePage";
 import ListPage from "./routes/listPage/listPage";
 import SinglePage from "./routes/singlePage/singlePage";
+import ProfilePage from "./routes/profilePage/profilePage";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
@@ -21,6 +22,10 @@ function App() {
         {
           path: "/:id",
           element: <SinglePage />,
+        },
+        {
+          path: "/profile",
+          element: <ProfilePage />,
         },
       ],
     },
